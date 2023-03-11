5192d6f90c5eb560c8aa1afcc19397314b68c7fa
gram <git@orsinium.dev>
2023-03-11 08:46:23 +0100

    try

--- netlify.sh
+++ netlify.sh
@@ -2,8 +2,7 @@
 # This script is used by netlify to the HTML page
 set -e
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
-export PATH=$PATH:~/go/bin
-echo $(go env GOPATH)
-dir ~/go
-dir ~/go/bin
+export GOPATH=$(go env GOPATH)
+export PATH=$PATH:$GOPATH/bin
+dir $GOPATH/bin
 ./bin/task --concurrency=1 render
