f26fe8bc8ed7a370df473fa75c52af2de728dcce
gram <git@orsinium.dev>
2023-03-11 09:00:49 +0100

    show path

--- netlify.sh
+++ netlify.sh
@@ -2,5 +2,5 @@
 # This script is used by netlify to the HTML page
 set -e
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
-export PATH=$PATH:~/.local/bin
+echo $PATH
 ./bin/task --concurrency=1 render
