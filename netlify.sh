c04c3ee81f4f63e5fd9f1c5fbba74e5bc99dbd9a
gram <git@orsinium.dev>
2023-03-11 08:54:25 +0100

    install enc from binary

--- netlify.sh
+++ netlify.sh
@@ -2,7 +2,5 @@
 # This script is used by netlify to the HTML page
 set -e
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
-export GOPATH=$(go env GOPATH)
-export PATH=$PATH:$GOPATH/bin
-dir $GOPATH/bin
+export PATH=$PATH:~/.local/bin
 ./bin/task --concurrency=1 render
