98fea16e138dbf2b2a0640402558f103dcf21005
gram <git@orsinium.dev>
2023-03-11 09:20:53 +0100

    set go path for enc on netlify

--- netlify.sh
+++ netlify.sh
@@ -2,5 +2,4 @@
 # This script is used by netlify to the HTML page
 set -e
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
-go env
-./bin/task --concurrency=1 render
+./bin/task render ENC=/opt/buildhome/.gimme_cache/gopath/bin/enc
