1c5a1954718df59d071934abb2f28498e2886545
gram <git@orsinium.dev>
2023-03-11 09:02:49 +0100

    debug

--- netlify.sh
+++ netlify.sh
@@ -2,5 +2,5 @@
 # This script is used by netlify to the HTML page
 set -e
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
-echo $PATH
+dir /opt/buildhome/.gimme/versions/go1.19.7.linux.amd64/bin
 ./bin/task --concurrency=1 render
