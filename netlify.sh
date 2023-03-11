a1d6b416da3074b8bfc791b78aec9bf481db1aa1
gram <git@orsinium.dev>
2023-03-11 08:44:38 +0100

    debug

--- netlify.sh
+++ netlify.sh
@@ -3,4 +3,7 @@
 set -e
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
 export PATH=$PATH:~/go/bin
-./bin/task render
+echo $(go env GOPATH)
+dir ~/go
+dir ~/go/bin
+./bin/task --concurrency=1 render
