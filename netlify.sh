871099b245913689912db30fb880cdd2cb5100d5
gram <git@orsinium.dev>
2023-03-11 09:07:50 +0100

    go env

--- netlify.sh
+++ netlify.sh
@@ -2,5 +2,5 @@
 # This script is used by netlify to the HTML page
 set -e
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
-dir /opt/buildhome/.gimme/versions/go1.19.7.linux.amd64/bin
+go env
 ./bin/task --concurrency=1 render
